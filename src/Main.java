c0c84a9ed6456ef46863c186487a95986a81e863
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-07 10:07:41 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=10;
+        float f =20.5f;
+        boolean b = true;
+        char a ='a';
+       String s="Ha noi";
+        System.out.println("int :"+ i);
+        System.out.println("float :"+ f);
+        System.out.println("boolean :"+ b);
+        System.out.println("char :"+a);
+        System.out.println("string :"+ s);
+
+    }
+}
